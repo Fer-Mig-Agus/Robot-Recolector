180a8037426bbf8b6760fd6cf86e239e96e5aa92
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2024-08-27 21:23:35 -0300

    Proyecto Terminado

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
+const validationType = (rows, cols) => {
+  if (isNaN(rows)) return false;
+  if (isNaN(cols)) return false;
+  return true
+}
+
+const validationRange = (rows, cols) => {
+  if (window.screen.width < 560) {
+    if (rows < 1 || rows > 7) return false;
+    if (cols < 1 || cols > 7) return false;
+    
+  } else {
+    if (rows < 1 || rows > 10) return false;
+    if (cols < 1 || cols > 10) return false;
+  }
+
+  return true
+}
+
 document.getElementById("createField").addEventListener("click", function () {
+
   const rows = parseInt(document.getElementById("rows").value);
   const cols = parseInt(document.getElementById("cols").value);
+  if (!validationType(rows, cols)) {
+    return alert("Debes de colocar valores validos");
+  }
+  if (!validationRange(rows, cols)) {
+    if (window.screen.width < 560) {
+      return alert("Debes colocar valores entre 1 y 7    :)");
+    }else{
+      return alert("Debes colocar valores entre 1 y 10    :)");
+    }
+
+  }
   createField(rows, cols);
 });
 
-const Reload=document.querySelector("#reload");
-const Main=document.querySelector("#main");
-const Actions=document.querySelector("#actions")
+const Reload = document.querySelector("#reload");
+const Controls = document.querySelector("#controls");
+const Actions = document.querySelector("#actions")
+const Trees = document.querySelector("#trees");
 
-Reload.addEventListener("click",(event)=>{
+
+Reload.addEventListener("click", (event) => {
   event.preventDefault();
   window.location.reload();
 })
@@ -16,11 +49,11 @@ Reload.addEventListener("click",(event)=>{
 let robotPosition = { x: 0, y: 0 };
 
 function createField(rows, cols) {
-  alert("Creando campo");
-  Main.style.display="none";
-  Actions.style.display="block";
+  Controls.style.display = "none";
+  Actions.style.display = "flex";
+  Trees.style.display = "flex";
   const field = document.getElementById("field");
-  field.style.display="grid";
+  field.style.display = "grid";
   field.innerHTML = "";
   field.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
   field.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
@@ -60,6 +93,7 @@ document.getElementById("startRobot").addEventListener("click", function () {
     if (!nextCell) {
       clearInterval(interval);
       alert("¡Robot ha recogido todas las frutas!");
+      window.location.reload();
       return;
     }
 
